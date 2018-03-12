37683f5bb05a2a62635fcb7799ec0171f8e5c156
mteverfresh <shererzach@gmail.com>
2018-03-12 18:25:00 -0400

    modified:   tunnel.py

--- tunnel/tunnel.py
+++ tunnel/tunnel.py
@@ -2,7 +2,12 @@ from sys                import *
 from cursesmenu         import *
 from cursesmenu.items   import *
 
+import time
 import csv
+import argparse
+import subprocess
+
+PATH = '/home/Zach/development/python/hosts.csv'
 
 class MainMenu:
 
@@ -10,27 +15,51 @@ class MainMenu:
     hosts = {}
     item_count = 0
 
-    def __init__(self): 
+    def __init__(self, args): 
 
-        with open('/home/Zach/development/python/tunnel/hosts.csv', mode = 'r') as hostfile:
+        with open(PATH, mode = 'r') as hostfile:
             reader  = csv.reader(hostfile)
             hosts   = {rows[0]:rows[1] for rows in reader}
 
         for host, address in hosts.items():
-            tunnel = CommandItem(host, "ssh -Y" + address)
+            if '-a' in args:
+                tunnel = CommandItem(host, "ssh -" + args + " " + address)
+            else:
+                tunnel = CommandItem(host, "ssh " + address)
+
             self.menu.append_item(tunnel)
             self.item_count += 1
         
         self.menu.show()
         return
 
-    def printTitle(self):
-        self.menu.screen.addstr(self.item_count + 5, 0, "  ______           __             ______                       __\n")
-        self.menu.screen.addstr(self.item_count + 6, 0, " /_  __/_  _______/ /_  ____     /_  __/_  ______  ____  ___  / /\n")
-        self.menu.screen.addstr(self.item_count + 7, 0, "  / / / / / / ___/ __ \/ __ \     / / / / / / __ \/ __ \/ _ \/ / \n")
-        self.menu.screen.addstr(self.item_count + 8, 0, " / / / /_/ / /  / /_/ / /_/ /    / / / /_/ / / / / / / /  __/ /  \n")        
-        self.menu.screen.addstr(self.item_count + 9, 0, "/_/  \__,_/_/  /_.___/\____/    /_/  \__,_/_/ /_/_/ /_/\___/_/   \n")
-        return
+def printVersion():
+    print("  ______           __             ______                       __")
+    print(" /_  __/_  _______/ /_  ____     /_  __/_  ______  ____  ___  / /")
+    print("  / / / / / / ___/ __ \/ __ \     / / / / / / __ \/ __ \/ _ \/ / ")
+    print(" / / / /_/ / /  / /_/ / /_/ /    / / / /_/ / / / / / / /  __/ /  ")        
+    print("/_/  \__,_/_/  /_.___/\____/    /_/  \__,_/_/ /_/_/ /_/\___/_/   ")
+    print("\nVersion 0.1\n")
+    return
 
 #start the menu
-turboTunnel = MainMenu()
+argparser = argparse.ArgumentParser(description = "Easily ssh to remote hosts")
+argparser.add_argument("-a", "--args", help="the ssh args to use when tunneling")
+argparser.add_argument("-v", "--version", action='store_true', help="show the version")
+argparser.add_argument("-n", "--new", nargs=2, help="add a new name/hostname+address pair to the hosts.csv file")
+
+rawargs = argparser.parse_args()
+args = vars(rawargs)
+
+if '-v' in args:
+    printVersion()
+    sys.exit()
+elif '-n' in args:
+    with open(PATH, mode = 'a') as hostfile:
+        writer = csv.writer(hostfile)
+        writer.writerow(args['-n'][0] + ", " + args['-n'][1])
+    sys.exit()
+else:
+    printVersion()
+    time.sleep(1)
+    turboTunnel = MainMenu(args)
